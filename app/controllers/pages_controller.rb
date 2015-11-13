dc9f214080aadd770f7503ca0464a1247a78b734
Rodrigo Vargas <rodrigovargas123@gmail.com>
2015-11-13 19:11:42 -0200

    Fix error when user is logged and access the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user, only: [:board]
-
+  before_filter :authenticate_user
+  layout "site", :only => :index
+  
   def index
+    if (@current_user)
+      redirect_to board_path
+    end
   end
 
   def board
