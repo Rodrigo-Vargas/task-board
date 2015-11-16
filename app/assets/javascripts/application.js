1088ef7e050cb752791fa3dfe4b0f16521e8a84c
Rodrigo Vargas <rodrigovargas123@gmail.com>
2015-11-16 21:24:46 -0200

    Added basic version of pomodoro timer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,64 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+window.taskBoard = window.taskBoard || {}
+
+taskBoard.initPomodoroControls = function()
+{
+  taskBoard.pomodoroTimerElement = $("#pomodoro-timer");
+
+  $("#pomodoro-button").click(function(){
+    if (taskBoard.pomodoroTimerElement.attr("status") == "stop")
+      taskBoard.startPomodoro();
+    else
+      taskBoard.stopPomodoro();
+  });
+}
+
+taskBoard.startPomodoro = function()
+{
+  taskBoard.pomodoroTimerElement.html("25:00");
+  taskBoard.pomodoroTimerElement.attr("status", "start");
+
+  setTimeout(function(){ 
+    taskBoard.pomodoroLoop(); }, 1000);
+
+  $("#pomodoro-button").html("Stop");
+}
+
+taskBoard.stopPomodoro = function()
+{
+  taskBoard.pomodoroTimerElement.attr("status", "stop");
+  $("#pomodoro-button").html("Start");
+}
+
+taskBoard.pomodoroLoop = function()
+{
+  var htmlDisplay = taskBoard.pomodoroTimerElement.html();
+  var statusElement = taskBoard.pomodoroTimerElement.attr("status");
+  
+  var time = htmlDisplay.split(":");
+  var minutes = time[0];
+  var seconds = time[1];
+
+  seconds--;
+
+  if (seconds < 0)
+  {
+    seconds = 59;
+    minutes--;
+  }
+  
+  if ((minutes > 0 || seconds > 0) && statusElement == "start")
+  {
+    taskBoard.pomodoroTimerElement.html(minutes + ":" + seconds);
+
+    setTimeout(function(){ 
+    taskBoard.pomodoroLoop(); }, 1000);
+  }
+  else
+  {
+    taskBoard.pomodoroTimerElement.html(minutes + ":" + seconds);
+  }
+}
